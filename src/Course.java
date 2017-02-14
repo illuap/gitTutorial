94c9711de5cccd4c676904c4c037d788594cf273
Paul Li <paul.j.li.97@gmail.com>
2017-02-13 20:13:58 -0500

    Added Course Class and made changes to Instructor and Student Class.

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Paul on 2/13/2017.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname,String description){
+        this.cname = cname;
+        this.description = description;
+    }
+}
